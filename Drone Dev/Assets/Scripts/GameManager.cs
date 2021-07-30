72c964cd91f7b982f2ef10b920a1bf4adc176b7e
FreshNico <sandovalnicolas27@yahoo.com>
2021-07-30 02:29:16 -0700

    Added Changing Scenes
    Disabled the switching drones and pickup. Added a function that changes scenes after 2 attempts at the "regular course".

--- Drone Dev/Assets/Scripts/GameManager.cs
+++ Drone Dev/Assets/Scripts/GameManager.cs
@@ -5,10 +5,37 @@ public class GameManager : MonoBehaviour
 {
    
     public GameObject completeLevelUI;
+    private static int levelCount = 0;
+    private float restartDelay = 2f;
 
     public void CompleteLevel()
     {
         completeLevelUI.SetActive(true);
+        Invoke("RestartCourse", restartDelay);
+        
+        
+
+
+
+    }
+
+    public void RestartCourse()
+
+    {
+        levelCount++;
+        Debug.Log("Attempt Number: " + levelCount);
+
+        if (levelCount == 2)
+        {
+
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+        else
+        {
+            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+           
+        }
     }
 
 
